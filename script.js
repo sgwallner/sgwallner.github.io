8b97cec378861a3f0ab341a4ea7b253d20d6edbd
Andrew Dassonville <dassonville.andrew@gmail.com>
2020-09-06 12:49:41 -0700

    Write script output to file

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ const fs = require('fs')
 
 const files = fs.readdirSync('sample_image').filter(i => i.toLowerCase() !== '.ds_store')
 
-console.log(files.map(i => `{ stimulus: "sample_image/${i}" },`).join('\n'))
+const sampleImages = files.map(i => `{ stimulus: "sample_image/${i}" },`).join('\n')
 
+fs.writeFileSync('sample_images.txt', sampleImages)
