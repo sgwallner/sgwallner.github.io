e762ab8063c2fa992c4b6c7655c600931f3c9db1
Andrew Dassonville <dassonville.andrew@gmail.com>
2020-09-06 12:47:17 -0700

    Update script

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 
-const files = fs.readdirSync('sample_image')
+const files = fs.readdirSync('sample_image').filter(i => i.toLowerCase() !== '.ds_store')
 
 console.log(files.map(i => `{ stimulus: "sample_image/${i}" },`).join('\n'))
 
